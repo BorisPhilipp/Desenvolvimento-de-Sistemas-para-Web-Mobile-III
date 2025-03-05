6e811f135466ee8090a7cfb580e6eee0d6c903c4
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-05 15:24:26 -0300

    implementacao de funções

---
+++ 02 - Atividade/functions.php
@@ -0,0 +1,44 @@
+<?php
+    function cadastrarAlunos($nome_aluno, $nota_aluno){
+
+        if(!empty($nome_aluno) && !empty($nota_aluno)){
+            $arquivo_notas = fopen("notas.txt", "a");
+            fwrite($arquivo_notas, "$nome_aluno,$nota_aluno\n");
+            fclose($arquivo_notas);
+    
+            return true;
+        } else{
+            return false;
+    }
+    }
+
+    function listarAlunos(){
+        if(file_exists("notas.txt")){
+            $arquivo_notas = fopen("notas.txt", "r");
+            $alunos = [];
+
+            while(!feof($arquivo_notas)){
+                $linha = fgets($arquivo_notas);
+                if(!empty($linha)){
+                    list($nome_aluno,$nota_aluno) = explode(",", $linha);
+                    $alunos[] = ["nome" => $nome, "nota" => floatval($nota_aluno)];
+                }
+            }
+            fclose($arquivo_notas);
+
+            return $alunos;
+        } else{
+            return [];
+        }
+    }
+
+    function calcularMedia($alunos){
+        if(count($alunos)>0){
+            $totalNotas = array_sum(array_column($alunos, "nota"));
+            $quantidadeAlunos = count($alunos);
+            return $totalNotas / $quantidadeAlunos;
+        } else{
+            return 0;
+        }
+    }
+?>
