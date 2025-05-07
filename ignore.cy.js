3683af2b91829a084654e53275b3fbb82c1e846e
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2025-05-06 21:58:57 -0300

    Update ignore.cy.js

--- ignore.cy.js
+++ ignore.cy.js
@@ -1,8 +1,8 @@
-//nome original teste.cy.js
+//teste.cy.js
 
 describe('Teste de Registro de Usuário', () => {
   // 1. Definindo a baseUrl corretamente
-  const baseUrl = 'http://172.32.90.26:8000'; // ou seu URL base
+  const baseUrl = 'http://172.32.90.38:8000'; // ou seu URL base
 
   it('Deve registrar um novo usuário com sucesso', () => {
     // 2. Dados do teste
@@ -14,7 +14,7 @@ describe('Teste de Registro de Usuário', () => {
     };
 
     const item = {
-      produto: 'Cachaça',
+      produto: 'Graxa em Pó',
       valor: '10,00',
       vencimento: '2001-09-11',
       situacao: 'Paga'
@@ -22,13 +22,13 @@ describe('Teste de Registro de Usuário', () => {
 
     // 3. Visita a página de registro
     cy.visit(`${baseUrl}/register`);
-    cy.screenshot('1-pagina-registro-inicial')
     // 4. Preenche o formulário (ajuste os seletores conforme seu HTML real)
-     
+    
     cy.get('input[name="name"]').should('be.visible').type(user.name);
     cy.get('input[name="email"]').type(user.email);
     cy.get('input[name="password"]').type(user.password);
     cy.get('input[name="password_confirmation"]').type(user.password);
+    cy.screenshot('1-pagina-registro-inicial');
 
     cy.get('button[type="submit"]').click();
     // 5. Submete o formulário
@@ -42,15 +42,19 @@ describe('Teste de Registro de Usuário', () => {
 
 
     //Adicionar Produto
-    cy.visit(`${baseUrl}/create-conta`);
+    cy.contains('Cadastrar');
     cy.get('input[name="nome"]').should('be.visible').type(item.produto);
     cy.get('input[name="valor"]').type(item.valor);
     cy.get('input[name="vencimento"]').type(item.vencimento);
     cy.get('select[name="situacao_conta_id"]').select(item.situacao).should('have.value', '1');
     cy.screenshot('3-pagina-cadastro-produto');
+
     cy.get('button').contains('Cadastrar').click();
 
     //Editar Produto
+    cy.visit(`${baseUrl}/index-conta`);
+
+
 
 
     // Verifica se não há mensagens de erro
